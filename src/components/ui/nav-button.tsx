f8d85f27ac8b0b7ce615be57eff98d2fceab4f3c
Falk Bruskeland <falk.bruskeland@gmail.com>
2024-08-03 11:30:59 +0200

    Improve styling of buttons

--- src/components/ui/nav-button.tsx
+++ src/components/ui/nav-button.tsx
@@ -5,16 +5,28 @@ import Link from "next/link";
 interface NavButtonProps {
   label: string;
   href: string;
+  color?: string;
   icon?: React.ReactNode;
   children?: React.ReactNode;
 }
 
-const NavButton = ({ label, href, icon, children }: NavButtonProps) => {
+const NavButton = ({ label, href, color, icon, children }: NavButtonProps) => {
+  const colorVariants: { [id: string]: string } = {
+    blue: "bg-blue-500 hover:bg-blue-500/90",
+    red: "bg-red-500 hover:bg-red-500/90",
+    green: "bg-green-500 hover:bg-green-500/90",
+    fuchsia: "bg-fuchsia-500 hover:bg-fuchsia-500/90",
+    teal: "bg-teal-500 hover:bg-teal-500/90",
+    orange: "bg-orange-500 hover:bg-orange-500/90",
+  };
+
   return (
     <Link href={href} passHref>
-      <Button className="w-full group" size="lg">
-        {icon && icon}
-        {label}
+      <Button className={`w-full group ${colorVariants[color!]}`} size="lg">
+        <div className="flex gap-1 items-center">
+          {icon && icon}
+          {label}
+        </div>
         {children}
         <ArrowRight
           size={20}
