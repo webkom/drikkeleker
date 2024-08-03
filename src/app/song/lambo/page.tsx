4f9ed549f98287b81b99882ca8b4a46bc4c16a61
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2024-08-03 21:10:47 +0200

    Add instructions to lambo

--- src/app/song/lambo/page.tsx
+++ src/app/song/lambo/page.tsx
@@ -3,20 +3,24 @@ import BackButton from "@/components/back-button";
 
 const lambo = [
   [
+    "Tilskuere synger:",
     "Se der står en fyllehund, mine herrer lambo.",
     "Sett nu flasken for din munn, mine herrer lambo.",
     "Se hvordan den dråpen vanker ned ad halsen på den dranker",
-    "Lambo, lambo, mine herrer lambo",
+    ":/: Lambo, lambo, mine herrer lambo :/: (Synges til dranker har drukket opp)",
   ],
   [
+    "Dranker synger:",
     "Jeg mitt glass utdrukket har, mine herrer lambo.",
     "Se der fins ei dråpen kvar, mine herrer lambo.",
     "Som bevis der på jeg vender, flasken på dens rette ende.",
+    "(Pause mens drankeren snur glass/flaske over hodet)",
   ],
   [
+    "Tilskuere synger:",
     "Lambo, lambo, mine herrer lambo",
-    "Hun/han kunne kunsten hun/han var et jævla fyllesvin.",
-    "Så går vi til nestemann og ser hva hun/han formår.",
+    "Han/hun kunne kunsten han/hun var et jævla fyllesvin.",
+    "Så går vi til nestemann og ser hva han/hun formår.",
     "(Alternativ: Så går vi til baren og sjenker oss en tår.)",
   ],
 ];
