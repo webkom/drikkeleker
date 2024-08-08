5c44c345ed8e8b79c80456f347bd0b8bf4b43bb2
Falk Bruskeland <falk.bruskeland@gmail.com>
2024-08-08 20:15:50 +0200

    Add Six Minutes

---
+++ src/app/six-minutes/ProgessBar.tsx
@@ -0,0 +1,17 @@
+type ProgressBarProps = {
+  value: number;
+  maxValue: number;
+}
+
+const ProgressBar = ({value, maxValue}: ProgressBarProps) => {
+  return (
+    <div className="w-full text-center" >
+      <div className="w-full h-6 rounded-full bg-gray-200">
+      	<div className="h-6 rounded-full bg-orange-500" style={{ width: `${100*(1-value/maxValue)}%`}}></div>
+      </div>
+      <span>00:{Math.ceil(maxValue-value)}</span>
+    </div>
+  );
+}
+
+export default ProgressBar;
---
+++ src/app/six-minutes/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { CirclePause, CirclePlay, Pause, Play, SkipForward } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import songs from './songs.json';
+import BeerContainer from "@/components/beer/beer-container";
+import { Button } from "@/components/ui/button";
+import ProgressBar from "./ProgessBar";
+
+const URL = "https://atlasimagesgallery.blob.core.windows.net/drikkelek";
+
+const Card = ({children}) => {
+  return (
+    <div className="flex flex-col items-center bg-white h-[500px]">
+      {children}
+    </div>
+  )
+}
+
+export default function SixMinutes() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const [currentSong, setCurrentSong] = useState(0);
+  const [currentPlayTime, setCurrentPlayTime] = useState(0);
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.play();
+    }
+    else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  const onPlaying = () => {
+    const ct = audioRef.current!.currentTime;
+    setCurrentPlayTime(ct);
+  }
+
+  const nextSong = () => {
+    setCurrentSong(current => (current + 1) % songs.length);
+    setIsPlaying(false);
+    setIsFlipped(false);
+  }
+
+  return (
+    <main className="w-screen h-screen" >
+      <audio ref={audioRef} src={`${URL}/${songs[currentSong].filename}`} onTimeUpdate={onPlaying} />
+      <BeerContainer>
+        <ReactCardFlip isFlipped={isFlipped}>
+          <Card>
+            {(currentSong+1)}
+            {isPlaying ? <CirclePause size={100} onClick={() => setIsPlaying(false)} />  : <CirclePlay size={100} onClick={() => setIsPlaying(true) } />}
+            <ProgressBar value={currentPlayTime} maxValue={20} />
+          </Card>
+          <Card>
+            {(currentSong+1)}
+            {songs[currentSong].title}
+            {songs[currentSong].artist}
+          </Card>
+        </ReactCardFlip>
+        <Button onClick={() => setIsFlipped(!isFlipped)} >Snu</Button>
+        <Button onClick={nextSong}><SkipForward/> Skip</Button>
+      </BeerContainer>
+    </main>
+  );
+}
