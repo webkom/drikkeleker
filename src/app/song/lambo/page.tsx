6a953c77c1f7155e5acf9b0ed60d07f2d2584d6e
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2024-08-11 12:52:22 +0200

    Add keys

--- src/app/song/lambo/page.tsx
+++ src/app/song/lambo/page.tsx
@@ -10,23 +10,28 @@ const lambo = [
     "Sett nu flasken for din munn, mine herrer lambo.",
     "Se hvordan den dråpen vanker ned ad halsen på den dranker",
     <>
-      <b>:/:</b> Lambo, lambo, mine herrer lambo <b>:/:</b>{" "}
+      <b key="Refreng start">:/:</b> Lambo, lambo, mine herrer lambo{" "}
+      <b key="Refreng slutt">:/:</b>{" "}
     </>,
-    <i>(Synges til dranker har drukket opp)</i>,
+    <i key="Tilskuer slutt">(Synges til dranker har drukket opp)</i>,
   ],
   [
     <b key="Dranker">Dranker synger</b>,
     "Jeg mitt glass utdrukket har, mine herrer lambo.",
     "Se der fins ei dråpen kvar, mine herrer lambo.",
     "Som bevis der på jeg vender, flasken på dens rette ende.",
-    <i>(Pause mens drankeren snur glass/flaske over hodet)</i>,
+    <i key="Dranker slutt">
+      (Pause mens drankeren snur glass/flaske over hodet)
+    </i>,
   ],
   [
     <b key="Tilskuere 2">Tilskuere synger</b>,
     "Lambo, lambo, mine herrer lambo",
     "Han/hun kunne kunsten han/hun var et jævla fyllesvin.",
     "Så går vi til nestemann og ser hva han/hun formår.",
-    <i>(Alternativt&#58; Så går vi til baren og sjenker oss en tår.)</i>,
+    <i key="Slutt">
+      (Alternativt&#58; Så går vi til baren og sjenker oss en tår.)
+    </i>,
   ],
 ];
 
