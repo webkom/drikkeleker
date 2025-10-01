cd837ba17f2f468651430a7e0c65de9b94ec66ba
Viljen Apalset Vassbø <90694640+Viljen789@users.noreply.github.com>
2025-10-01 17:35:13 +0200

    Group room (#40)
    * Merge remote-tracking branch 'origin/group-room' into group-room
     Prettier
    
    # Conflicts:
    #	src/app/custom-questions/GameRoomClient.tsx
    #	src/app/custom-questions/Lobby.tsx
    #	src/app/custom-questions/Room.tsx
    #	src/app/custom-questions/page.tsx
    
    * Dice error fix
    # Conflicts:
    #	src/app/custom-questions/GameRoomClient.tsx
    #	src/app/custom-questions/Lobby.tsx
    #	src/app/custom-questions/Room.tsx
    #	src/app/custom-questions/page.tsx
    
    * json
    # Conflicts:
    #	src/app/custom-questions/GameRoomClient.tsx
    #	src/app/custom-questions/Lobby.tsx
    #	src/app/custom-questions/Room.tsx
    #	src/app/custom-questions/page.tsx
    
    * origin *
    # Conflicts:
    #	src/app/custom-questions/GameRoomClient.tsx
    #	src/app/custom-questions/Lobby.tsx
    #	src/app/custom-questions/Room.tsx
    #	src/app/custom-questions/page.tsx

--- gameroom-backend/server.js
+++ gameroom-backend/server.js
@@ -9,11 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://drikkeleker.coolify.webkom.dev",
-      "https://drikkeleker.abakus.no",
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
