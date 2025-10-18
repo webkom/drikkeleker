ec28ed01da740097e11f1f9f865169dc1059523f
Viljen <viljen.vassbo@gmail.com>
2025-10-18 14:09:44 +0200

    Fix input bug and stepper direction

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -257,7 +257,7 @@ function SlideTransition({
 
 const stepVariants: Variants = {
   enter: (dir: number) => ({
-    x: dir >= 0 ? "-100%" : "100%",
+    x: dir >= 0 ? "100%" : "-100%",
     opacity: 0,
   }),
   center: {
@@ -265,7 +265,7 @@ const stepVariants: Variants = {
     opacity: 1,
   },
   exit: (dir: number) => ({
-    x: dir >= 0 ? "50%" : "-50%",
+    x: dir >= 0 ? "-50%" : "50%",
     opacity: 0,
   }),
 };
